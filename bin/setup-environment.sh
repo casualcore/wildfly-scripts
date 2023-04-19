d140b6f5c21162f300a4842c09aba33f8d66d293
Mathias Creutz <mathiascreutz@yahoo.se>
2023-04-19 17:18:20 +0200

    works

--- bin/setup-environment.sh
+++ bin/setup-environment.sh
@@ -2,10 +2,13 @@
 #-*- coding: utf-8-unix -*-
 export JBOSS_HOME=${WILDFLY_HOME:-~/wildfly}
 export WILDFLY_BIN=${JBOSS_HOME}/bin
-export CASUAL_JCA_HOME=~/casual-java
-export CASUAL_VERSION=2.2.7
-export CASUAL_CALLER_VERSION=2.2.5
-export CASUAL_TEST_VERSION=1.0.2
-export CASUAL_OUTBOUND_POOLS='pool_name=my-pool-name,jndi_name=eis/casualConnectionFactory,min_pool_size=20,initial_pool_size=20,max_pool_size=20,port_number=7771,host_name=127.0.0.1'
+export CASUAL_ARTIFACTS_HOME=${CASUAL_ARTIFACTS_HOME:-~/casual-java-deps-from-maven-central}
+export CASUAL_TEST_HOME=${CASUAL_TEST_HOME:-~/casual-java-test-apps/casual-java-test-app}
+export CASUAL_VERSION=3.2.22
+export CASUAL_CALLER_VERSION=3.2.8
+export CASUAL_TEST_VERSION=2.0.2
+export GSON_VERSION=2.10.1
+export CASUAL_OUTBOUND_POOLS='pool_name=my-pool-name,jndi_name=eis/casualConnectionFactory,min_pool_size=20,initial_pool_size=20,max_pool_size=20,port_number=7772,host_name=10.108.120.202'
 export CASUAL_INBOUND_STARTUP_MODE=trigger
-export CASUAL_FIELD_TABLE=$CASUAL_JCA_HOME/casual/casual-api/src/test/resources/casual-fields.json
+export CASUAL_FIELD_TABLE=${CASUAL_FIELD_TABLE:-~/etc/casual-fields.json}
+export CASUAL_CONFIG_FILE=${CASUAL_CONFIG_FILE:-~/etc/casual-config.json}
